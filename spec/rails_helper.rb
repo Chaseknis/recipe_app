688e23b991561f6eab27d7bae662e4dee66b6637
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-05-25 17:24:03 +0100

    Add some helper code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'devise'
 require 'spec_helper'
+require 'shoulda/matchers'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -61,3 +69,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+Capybara.default_driver = :selenium_chrome
