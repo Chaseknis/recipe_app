88c8ce5e5c3ba5886beb6e9d950e6e60c3c9d5a0
chaseknis <chase6th@gmail.com>
2023-05-26 15:07:01 +0200

    Run linters for errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,14 +1,14 @@
 class ShoppingListsController < ApplicationController
-    def index
-      @user = current_user
-      @recipes = Recipe.includes(recipe_foods: :food).where(user: @user)
-  
-      @ingredients = RecipeFood.includes(food: :recipe_foods)
-                               .where(recipe: @recipes)
-                               .group_by { |ingredient| ingredient.food.name }
-  
-      @total_price = @ingredients.sum do |_food, ingredients|
-        ingredients.sum { |ingredient| ingredient.food.price * ingredient.quantity }
-      end
+  def index
+    @user = current_user
+    @recipes = Recipe.includes(recipe_foods: :food).where(user: @user)
+
+    @ingredients = RecipeFood.includes(food: :recipe_foods)
+      .where(recipe: @recipes)
+      .group_by { |ingredient| ingredient.food.name }
+
+    @total_price = @ingredients.sum do |_food, ingredients|
+      ingredients.sum { |ingredient| ingredient.food.price * ingredient.quantity }
     end
-  end  
+  end
+end
