c892ef58fb17ecbc87fd0b09b2c86e6218dd075a
chaseknis <chase6th@gmail.com>
2023-05-26 15:06:22 +0200

    Update development.rb file Update shopping_lists_controller.rb file to fix the n+1 query issues

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,13 +1,14 @@
 class ShoppingListsController < ApplicationController
-  def index
-    @user = current_user
-    @recipe = Recipe.where(user: @user)
-    @ingredients = RecipeFood.includes(:food).where(recipe: @recipe).group_by { |ingredient| ingredient.food.name }
-
-    @total_price = @ingredients.map do |_food, ingredients|
-      ingredients.map do |ingredient|
-        ingredient.food.price * ingredient.quantity
-      end.sum
-    end.sum
-  end
-end
+    def index
+      @user = current_user
+      @recipes = Recipe.includes(recipe_foods: :food).where(user: @user)
+  
+      @ingredients = RecipeFood.includes(food: :recipe_foods)
+                               .where(recipe: @recipes)
+                               .group_by { |ingredient| ingredient.food.name }
+  
+      @total_price = @ingredients.sum do |_food, ingredients|
+        ingredients.sum { |ingredient| ingredient.food.price * ingredient.quantity }
+      end
+    end
+  end  
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.active_record.logger = Logger.new(STDOUT)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
