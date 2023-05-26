bb05385b9a09560b9184a88e3edb668e08532ddd
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-05-26 12:07:42 +0100

    Add untit test

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :recipe do
     name { 'Apple Cake' }
     description { 'This is cake with apple.' }
-    preparation_time { '1 hour' }
-    cooking_time { '1.5 hours' }
+    preparation_time { 1 }
+    cooking_time { 1 }
     public { true }
     user { association :user }
   end
